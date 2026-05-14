886cc30919c39aed1cf62f6b5d6e5c7d774a0e9c
Tes-program <teslimodumuyiwa@gmail.com>
2026-05-14 18:43:09 +0100

    chore: configure production deployment

--- careround-core/src/test/java/com/careround/scheduler/jobs/OutboxPollerJobTest.java
+++ careround-core/src/test/java/com/careround/scheduler/jobs/OutboxPollerJobTest.java
@@ -11,7 +11,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Pageable;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.ObjectMapper;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
